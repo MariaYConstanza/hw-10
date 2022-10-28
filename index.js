a280772763a0c7145f68f1373821ed1feb376aaa
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-10-28 14:52:07 -0400

    added html temp on engineer, intern and manager in index.js

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const render = require('./src/page-template.js');
 
 const teamMembers = [];
 
-const generateHTML = ({name, id, email, officeNumber, school, role}) => 
-// manager html layout 
+
+
+// manager html layout
+const generateManager = ({name, id, email, officeNumber, role}) => 
+
 `<!DOCTYPE html>
 <html>
   <head>
@@ -51,8 +54,82 @@ const generateHTML = ({name, id, email, officeNumber, school, role}) =>
   </body>
 </html>
 `
+
 // questions and answers for employee role and profile stats
 inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: 'What is your role in the company?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your employee ID number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your perferred email address?',
+    },
+    {
+      type: 'input',
+      name: 'office number',
+      message: 'What is your office number?',
+    },
+  ])
+
+
+
+  // engineer html layout
+  const generateEngineer = ({name, id, email, github, role}) => 
+  
+  `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Team Profile Generator</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+    </head>
+  
+    <body>
+   <header>
+      <div class="jumbotron jumbotron-fluid custom-jumbotron">
+          <div class="container">
+            <h1 class="display-4">My Team</h1>
+          </div>
+        </div>
+   </header>
+  
+   <main>
+      <div class="card custom-card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Employee Name ${name}</h5>
+            <p class="card-text">Employee position ${role}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID:${id}</li>
+            <li class="list-group-item">Email:${email}</li>
+            <li class="list-group-item">Github:${github}</li>
+          </ul>
+      </div>
+   </main>
+   <script src="./index.js"></script>
+    </body>
+  </html>
+  `
+
+  // inquirer questions and answers for engineer role
+  inquirer
   .prompt([
     {
       type: 'input',
@@ -79,6 +156,73 @@ inquirer
       name: 'github',
       message: 'Enter your GitHub Username.',
     },
+  ])
+
+
+
+  // intern html layout
+  const generateIntern = ({name, id, email, school, role}) => 
+  `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Team Profile Generator</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+    </head>
+  
+    <body>
+   <header>
+      <div class="jumbotron jumbotron-fluid custom-jumbotron">
+          <div class="container">
+            <h1 class="display-4">My Team</h1>
+          </div>
+        </div>
+   </header>
+  
+   <main>
+      <div class="card custom-card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Employee Name ${name}</h5>
+            <p class="card-text">Employee position ${role}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID:${id}</li>
+            <li class="list-group-item">Email:${email}</li>
+            <li class="list-group-item">University:${school}</li>
+          </ul>
+      </div>
+   </main>
+   <script src="./index.js"></script>
+    </body>
+  </html>
+  `
+
+  // inquirer questions anf answers for intern role
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: 'What is your role in the company?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your employee ID number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your perferred email address?',
+    },
     {
       type: 'input',
       name: 'school',
@@ -86,16 +230,7 @@ inquirer
     },
   ])
 
-  // engineer html layout
 
-  // inquirer questions and answers for engineer role
-
-  // intern html layout
-
-  // inquirer questions anf answers for intern role
-
-
-  
   .then((answers) => {
     const htmlPageContent = generateHTML(answers);
 
