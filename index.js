837e0da8c335a6afa387b62c3b41804dced1f5da
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-10-28 10:24:03 -0400

    formated html into index.js

--- index.js
+++ index.js
@@ -12,6 +12,45 @@ const render = require('./src/page-template.js');
 
 const teamMembers = [];
 
+const generateHTML = ({}) => 
+`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Team Profile Generator</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+  </head>
+
+  <body>
+ <header>
+    <div class="jumbotron jumbotron-fluid custom-jumbotron">
+        <div class="container">
+          <h1 class="display-4">My Team</h1>
+        </div>
+      </div>
+ </header>
+
+ <main>
+    <div class="card custom-card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Employee Name</h5>
+          <p class="card-text">Employee position</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID:</li>
+          <li class="list-group-item">Email:</li>
+          <li class="list-group-item">GitHub:</li>
+        </ul>
+    </div>
+ </main>
+ <script src="./index.js"></script>
+  </body>
+</html>
+`
+
 fs.writeFile()
 
 // function for creating manager - inquirer questions
